41aa1e276ab433a99fbba64fb5106bf61c0cb510
John Jiang <jiangc@cambridgeenglish.org>
2021-04-22 07:58:22 +0100

    Added health endpoint and e2e test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,18 +2,25 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { AppServiceImpl } from './app.service';
 import { Institution } from './entities/Institution';
 
-@Controller('/institutions')
+@Controller('/')
 export class AppController {
   constructor(private readonly appService: AppServiceImpl) {}
 
-  @Get('/')
+  // @TODO, this health endpoint should also check database health status as well
+  @Get('/health')
+  public async health(): Promise<boolean> {
+    return true;
+  }
+
+  // @TODO this route can be in institutions controller
+  @Get('/institutions')
   public async getInstitutions(): Promise<Institution[]> {
     const result = this.appService.findAllInstitutions();
     return result;
   }
 
-  // @TODO this route can be in subject controller
-  @Get('/subjects/:subject')
+  // @TODO this route can be in institutions controller
+  @Get('/institutions/subjects/:subject')
   public async getInstitutionsBySubject(
     @Param('subject') subject: string,
   ): Promise<Institution[]> {
