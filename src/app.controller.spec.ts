a3a2766e161b82d05a3958d319e410d150dde2d9
John Jiang <jiangc@cambridgeenglish.org>
2021-04-22 07:32:50 +0100

    Added unit tests for service and controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,13 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import fixtures from '../test/fixtures/institutions';
 import { AppController } from './app.controller';
 import { AppServiceImpl } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
   let mockAppService: jest.Mock;
+
   beforeEach(async () => {
     mockAppService = jest.fn(() => ({
-      findAllInstitutions: jest.fn().mockResolvedValue([]),
+      findAllInstitutions: jest.fn().mockResolvedValue(fixtures),
+      findInstitutionBySubject: jest.fn().mockResolvedValue(fixtures),
     }));
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
@@ -22,13 +25,21 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return list all institutions', async () => {
+  describe('Institutions', () => {
+    it('should return all institutions', async () => {
       // Act
       const result = await appController.getInstitutions();
 
       // Assert
-      expect(result).toEqual([]);
+      expect(result).toEqual(fixtures);
+    });
+
+    it('should return all institutions by subjects', async () => {
+      // Act
+      const result = await appController.getInstitutionsBySubject('Biology');
+
+      // Assert
+      expect(result).toEqual(fixtures);
     });
   });
 });
--- src/app.service.spec.ts
+++ src/app.service.spec.ts
@@ -1,18 +1,21 @@
 import { Test } from '@nestjs/testing';
+import fixtures from '../test/fixtures/institutions';
 import { AppService, AppServiceImpl } from './app.service';
 import { DataAccessImpl } from './dataAccess.service';
 
 describe('App Service', () => {
   let appService: AppService;
-  let dataAccess: DataAccessImpl;
   let findAllMock: jest.Mock;
+  let findByMock: jest.Mock;
   let mockDataAccess: jest.Mock;
 
   beforeEach(async () => {
     findAllMock = jest.fn();
+    findByMock = jest.fn();
 
     mockDataAccess = jest.fn(() => ({
       findAll: findAllMock,
+      findBySubject: findByMock,
     }));
 
     const module = await Test.createTestingModule({
@@ -26,18 +29,30 @@ describe('App Service', () => {
     }).compile();
 
     appService = module.get<AppServiceImpl>(AppServiceImpl);
-    dataAccess = module.get<DataAccessImpl>(DataAccessImpl);
   });
 
   it('should find all institutions', async () => {
     // Assign
-    findAllMock.mockResolvedValue([]);
+    findAllMock.mockResolvedValue(fixtures);
 
     // Act
     const results = await appService.findAllInstitutions();
 
     // Assert
-    expect(dataAccess.findAll).toBeCalled();
-    expect(results).toEqual([]);
+    expect(findAllMock).toBeCalled();
+    expect(results).toEqual(fixtures);
+  });
+
+  it('should find all institutions by subject', async () => {
+    // Assign
+    const [, ...findByFixtures] = fixtures;
+    findByMock.mockResolvedValue(findByFixtures);
+
+    // Act
+    const results = await appService.findInstitutionBySubject('Biology');
+
+    // Assert
+    expect(findByMock).toBeCalled();
+    expect(results).toEqual(findByFixtures);
   });
 });
